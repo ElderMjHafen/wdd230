9dba08a1c2b0564905fe15b419045710e2ec49e1
ElderMjHafen <eldermjhafen@byui.edu>
2024-01-17 10:01:44 -0600

    Fixed issies on the front page

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,2 +1,5 @@
+const currentY = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentY + " Michael J. Hafen, Texas";
+
 const oLastModif = document.lastModified;
-document.getElementById("lastModified").textContent = "Last Modified on " + new Date().toDateString("en-US", oLastModif);
+document.getElementById("lastModified").textContent = "Last Modified on " + oLastModif;
