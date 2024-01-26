483dea8a110e615bd20781b1784da8f3b9375d6b
ElderMjHafen <eldermjhafen@byui.edu>
2024-01-25 20:17:44 -0600

    Worked on Home Page Enhancement

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,3 +3,11 @@ document.getElementById("currentYear").textContent = currentY + " Michael J. Haf
 
 const oLastModif = document.lastModified;
 document.getElementById("lastModified").textContent = "Last Modified on " + oLastModif;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
