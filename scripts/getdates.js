ec48eb38e94ae87bba05197a1a6fb6614938c4c4
ElderMjHafen <eldermjhafen@byui.edu>
2024-01-26 12:32:27 -0600

    Worked on the Site Plan

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,13 +1,22 @@
+/****
+if (window.matchMedia != (window.matchMedia("(only screen and max-width: 600px)").matches)) {
+	const currentY = new Date().getFullYear();
+	document.getElementById("currentYear").textContent = currentY + "Michael J. Hafen, Texas";
+
+	const oLastModif = document.lastModified;
+	document.getElementById("lastModified").textContent = "Last Modified on " + oLastModif;
+}
+else if (window.matchMedia == (window.matchMedia("(only screen and max-width: 600px)").matches)) {
+	const currentY = new Date().getFullYear();
+	document.getElementById("currentYear").textContent = currentY + " Michael J. Hafen, Texas |";
+
+	const oLastModif = document.lastModified;
+	document.getElementById("lastModified").textContent = "| Last Modified on " + oLastModif;
+}
+/*/
 const currentY = new Date().getFullYear();
-document.getElementById("currentYear").textContent = currentY + " Michael J. Hafen, Texas |";
+	document.getElementById("currentYear").textContent = currentY + " Michael J. Hafen, Texas |";
 
-const oLastModif = document.lastModified;
-document.getElementById("lastModified").textContent = "| Last Modified on " + oLastModif;
-
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
-
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
-});
+	const oLastModif = document.lastModified;
+	document.getElementById("lastModified").textContent = "| Last Modified on " + oLastModif;
+/** */
