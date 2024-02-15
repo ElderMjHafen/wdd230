162ce470cf1636dcb4bab2bb465715bda9aaad35
ElderMjHafen <eldermjhafen@byui.edu>
2024-02-15 11:53:49 -0600

    Add conditions to weather section

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,6 +1,12 @@
-var temp= 72;
+var temp= 42;
 var wSpeed= 3.5;
-var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
 
-var windChill= Math.round(windChill);
-document.getElementById("windChill").innerHTML= windChill;
+if (temp <= 50 & wSpeed > 3.0) {
+    var windChill= (35.74 + (0.6215 * temp))-(35.75 * Math.pow(wSpeed,0.16)) + (0.4275*temp*Math.pow(wSpeed,0.16));
+    var windChill= Math.round(windChill);
+
+    document.getElementById("windChill").innerHTML= windChill;
+}
+else {
+    document.getElementById("windChill").innerHTML= "N/A";
+}
