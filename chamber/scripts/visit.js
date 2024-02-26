0ad0b55d1b160d6902a4f3b3d163f0e7ab9c0ffa
ElderMjHafen <eldermjhafen@byui.edu>
2024-02-26 17:45:00 -0600

    Got message to visitor to work properly as required in assignment instruction.

--- chamber/scripts/visit.js
+++ chamber/scripts/visit.js
@@ -1,33 +1,23 @@
 const visitsDisplay = document.querySelector(".visits");
 
-let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+let storedDate = window.localStorage.getItem("lastVisitedDate");
+const todayDate = Date.now();
 
-if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
-} else {
+if (!storedDate) {
 	visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
-}
-
-numVisits++;
-
-localStorage.setItem("numVisits-ls", numVisits);
-
-
-
-/*const msToDays = 84600000/1000 * 60/60 *60/60 *24;*/
-
-if (diffDays == 0) {
-	visitsDisplay.textContent = "Back so soon! Awesome!";
-}
-else if (diffDays == 1) {
-	visitsDisplay.textContent = "You last visited " + diffDays + " day.";
-}
-else if (diffDays > 1){
-	visitsDisplay.textContent = "You last visited " + diffDays + " days.";
+	storedDate = window.localStorage.setItem("lastVisitedDate", Date.now());
 }
 else {
-	visitsDisplay.textContent = "Welcome! Let us know if you have any questions.";
+	const diffDays = todayDate - storedDate;
+	const dateDiff =  Math.round(diffDays/(1000*60*60*24));
+	
+	if (dateDiff < 1) {
+		visitsDisplay.textContent = "Back so soon! Awesome!";
+	}
+	else if (dateDiff == 1) {
+		visitsDisplay.textContent = "You last visited " + dateDiff + " day ago.";
+	}
+	else if (dateDiff > 1){
+		visitsDisplay.textContent = "You last visited " + dateDiff + " days ago.";
+	}
 }
-diffDays = 0;
-
-localStorage.setItem("lastVesitedTime", diffDays);
