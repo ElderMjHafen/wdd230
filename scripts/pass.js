8c2f1a68d2302066c74b4e1722028d6d91e89915
ElderMjHafen <eldermjhafen@byui.edu>
2024-03-02 16:41:40 -0600

    Got the password and comfirm password to function

--- scripts/pass.js
+++ scripts/pass.js
@@ -1,20 +1,13 @@
-var kp1 = document.querySelector("#keyphrase");
-const kp2 = document.querySelector("#keyphrase2");
-const message = document.querySelector("#formmessage");
+var password = document.getElementById("password")
+  , confirm_password = document.getElementById("confirm_password");
 
-kp2.addEventListener("focusout", checkSame);
-
-// This should be refactored.
-function checkSame() {
-	if (kp1.value !== kp2.value) {
-		message.textContent = "❗Key Phrases DO NOT MATCH!";
-		message.style.visibility = "show";
-		kp2.style.backgroundColor = "#fff0f3";
-		kp2.value = "";
-		kp2.focus();
-	} else {
-		message.style.display = "none";
-		kp2.style.backgroundColor = "#fff";
-		kp2.style.color = "#000";
-	}
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
 }
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
