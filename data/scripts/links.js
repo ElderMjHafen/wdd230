9d43d0da5cf6c120d96ef31be4c76b14aafe67d7
ElderMjHafen <eldermjhafen@byui.edu>
2024-03-07 12:46:23 -0600

    Added JSON to wdd230 root folder

---
+++ data/scripts/links.js
@@ -0,0 +1,33 @@
+
+const url = "json/links.js";
+const baseURL = "https://eldermjhafen.github.io/wdd230/";
+const linksURL = "https://eldermjhafen.github.io/wdd230/data/links.json";
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data);
+}
+
+getLinks();
+
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    displayLinks(data);
+}
+
+const displayLinks = (weeks) => {
+    lessons.forEach((week) => {
+        // Create elements to add to the div.cards element
+        let card = document.createElement('section');
+        let link = document.createElement('h4');
+  
+        link.textContent = `${week.lesson}: ${week.links.tittle}`; // fill in the blank
+  
+        // Append the section(card) with the created elements
+        card.appendChild(link); //fill in the blank
+  
+        cards.appendChild(card);
+    });
+  }
