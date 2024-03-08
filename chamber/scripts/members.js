4cf590b0d25e512eada107aa629ec86f8aedc705
ElderMjHafen <eldermjhafen@byui.edu>
2024-03-08 11:39:53 -0600

    Fix spacing issues

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,27 +1,27 @@
-const url = '';
+const url = 'https://eldermjhafen.github.io/wdd230/chamber/data/members.json';
 const cards = document.querySelector('#cards');
 
-async function getProphetData() {
+async function getMembertData() {
     const response = await fetch(url);
     const data = await response.json();
     //console.table(data.prophets);
-    displayProphets(data.prophets); // note that we reference the prophets array of the JSON data object, not just the object
+    displayMembers(data.members); // note that we reference the prophets array of the JSON data object, not just the object
 }
   
-  getProphetData();
+getMembertData();
 
-const displayProphets = (prophets) => {
-  prophets.forEach((prophet) => {
+const displayMembers = (members) => {
+    members.forEach((member) => {
       // Create elements to add to the div.cards element
       let card = document.createElement('section');
       let fullName = document.createElement('h2');
       let portrait = document.createElement('img');
 
       // Build the h2 content out to show the prophet's full name
-      fullName.textContent = `${prophet.name} ${prophet.lastname}`; // fill in the blank
+      fullName.textContent = `${member.name} ${member.phone} ${member.phone}`; // fill in the blank
       // Build the image portrait by setting all the relevant attributes
-      portrait.setAttribute('src', prophet.imageurl);
-      portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`); // fill in the blank
+      portrait.setAttribute('src', member.imageurl);
+      portrait.setAttribute('alt', `Portrait of ${member.name} ${member.lastname}`); // fill in the blank
       portrait.setAttribute('loading', 'lazy');
       portrait.setAttribute('width', '340');
       portrait.setAttribute('height', '440');
