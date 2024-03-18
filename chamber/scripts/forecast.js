1e9d9964ba8eb7527091801e720742308c55f631
ElderMjHafen <eldermjhafen@byui.edu>
2024-03-18 10:41:04 -0500

    Updated the weather forecast

--- chamber/scripts/forecast.js
+++ chamber/scripts/forecast.js
@@ -14,6 +14,9 @@ const low3 = document.querySelector('#low3');
 const pre3 = document.querySelector('#pre3');
 const wind3 = document.querySelector('#windS3');
 
+var windChill2 = document.querySelector('#windChill2');
+var windChill3 = document.querySelector('#windChill3');
+
 const weatherIcon = document.querySelector('#weather-icon');
 const weatherIcon2 = document.querySelector('#weather-icon2');
 const weatherIcon3 = document.querySelector('#weather-icon3');
@@ -41,42 +44,76 @@ navigator.geolocation.getCurrentPosition(function(position) {
         console.log(data); // testing only
         console.log(`Next day:`, data.list[8]);
         console.log(`Following day:`, data.list[16]);
+
+        //Day 2
+        var ctemp2 = parseFloat(data.list[8].main.temp);
+        //ctemp2 = ctemp2 - 50; // test
+        console.log(`ctemp2:`, ctemp2)
+        var wSpeed2= parseFloat(data.list[8].wind.speed);
+
+        if (ctemp2 <= 50 & wSpeed2 > 3.0) {
+          windChill2 = (35.74 + (0.6215 * ctemp2))-(35.75 * Math.pow(wSpeed2,0.16)) + (0.4275*ctemp2*Math.pow(wSpeed2,0.16));
+          windChill2 = Math.round(windChill2);
+      
+          console.log(windChill2);
+      
+          document.getElementById("windChill2").innerHTML= windChill2 + `&deg;F`;
+        }
+        else {
+          document.getElementById("windChill2").innerHTML= `N/A`;
+        }
+
+        //Day 3
+        var ctemp3 = parseFloat(data.list[16].main.temp);
+        //ctemp3 = ctemp2 - 50; // test
+        console.log(`ctemp2:`, ctemp3)
+        var wSpeed3 = parseFloat(data.list[16].wind.speed);
+
+        if (ctemp3 <= 50 & wSpeed3 > 3.0) {
+          windChill3 = (35.74 + (0.6215 * ctemp2))-(35.75 * Math.pow(wSpeed3,0.16)) + (0.4275*ctemp3*Math.pow(wSpeed3,0.16));
+          windChill3 = Math.round(windChill3);
+      
+          console.log(windChill3);
+      
+          document.getElementById("windChill3").innerHTML = windChill3 + `&deg;F`;
+        }
+        else {
+          document.getElementById("windChill3").innerHTML = `N/A`;
+        }
         
         /*
-        function displayResults(data) {
-          console.log(`degree:`, data.wind.deg);
-          if (data.list[0].wind.deg == 0) {
-            var direction = `N`;
-          }
-          else if (data.list[0].wind.deg < 90) {
-            var direction = `NE`;
-          }
-          else if (data.list[0].wind.deg == 90) {
-            var direction = `E`;
-          }
-          else if (data.list[0].wind.deg < 180) {
-            var direction = `SE`;
-          }
-          else if (data.list[0].wind.deg == 180) {
-            var direction = `S`;
-          }
-          else if (data.list[0].wind.deg < 270) {
-            var direction = `SW`;
-          }
-          else if (data.list[0].wind.deg == 270) {
-            var direction = `W`;
-          }
-          else {
-            var direction = `NS`;
-          }
-          console.log(direction);
-          
-          wind.innerHTML = `${data.list[0].wind.speed} MPH ` + direction;
-          //wind2.innerHTML = `${data.list[8].wind.speed} MPH ` + direction;
-          //wind3.innerHTML = `${data.list[16].wind.speed} MPH ` + direction;
-        
-          console.log(data);
+        console.log(`degree:`, data.wind.deg);
+        if (data.list[0].wind.deg == 0) {
+          var direction = `N`;
         }
+        else if (data.list[0].wind.deg < 90) {
+          var direction = `NE`;
+        }
+        else if (data.list[0].wind.deg == 90) {
+          var direction = `E`;
+        }
+        else if (data.list[0].wind.deg < 180) {
+          var direction = `SE`;
+        }
+        else if (data.list[0].wind.deg == 180) {
+          var direction = `S`;
+        }
+        else if (data.list[0].wind.deg < 270) {
+          var direction = `SW`;
+        }
+        else if (data.list[0].wind.deg == 270) {
+          var direction = `W`;
+        }
+        else {
+          var direction = `NS`;
+        }
+        console.log(direction);
+        
+        wind.innerHTML = `${data.list[0].wind.speed} MPH ` + direction;
+        //wind2.innerHTML = `${data.list[8].wind.speed} MPH ` + direction;
+        //wind3.innerHTML = `${data.list[16].wind.speed} MPH ` + direction;
+      
+        console.log(data);
         /** */
 
         // Day 1
@@ -84,7 +121,7 @@ navigator.geolocation.getCurrentPosition(function(position) {
         high.innerHTML = `High: ${data.list[0].main.temp_max}&deg;F`;
         low.innerHTML = `Low: ${data.list[0].main.temp_min}&deg;F`;
         pre.innerHTML = `${data.list[0].main.humidity}%`;
-        wind.innerHTML = `${data.list[0].wind.speed} MPH `;
+        //wind.innerHTML = `${data.list[0].wind.speed} MPH `;
         const iconsrc = `https://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png`;
         let desc = data.list[0].weather[0].description;
         weatherIcon.setAttribute('src', iconsrc);
@@ -118,8 +155,6 @@ navigator.geolocation.getCurrentPosition(function(position) {
         captionDesc3.textContent = `${desc3}`;
 
         
-
-
         displayResults(data); // uncomment when ready
       } else {
           throw Error(await response.text());
@@ -129,8 +164,6 @@ navigator.geolocation.getCurrentPosition(function(position) {
         console.log(error);
     }
   }
-
-  
   apiFetch();
 });
 //*
@@ -162,7 +195,7 @@ function displayResults(data) {
   }
   console.log(direction);
   
-  //wind.innerHTML = `${data.wind.speed} MPH ` + direction;
+  wind.innerHTML = `${data.wind.speed} MPH ` + direction;
   //wind2.innerHTML = `${data.list[8].wind.speed} MPH ` + direction;
   //wind3.innerHTML = `${data.list[16].wind.speed} MPH ` + direction;
 
--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,7 +1,6 @@
 //const wind = document.querySelector('#windS');
 var windChill = document.querySelector('#windChill');
-var windChill2 = document.querySelector('#windChill2');
-var windChill3 = document.querySelector('#windChill3');
+
 
 //Get the current location
 /** */
@@ -22,6 +21,7 @@ navigator.geolocation.getCurrentPosition(function(position) {
           const data = await response.json();
           console.log(data); // testing only
 
+          //Day 1
           var ctemp = parseFloat(data.main.temp);
           //ctemp = ctemp - 50; // test
           console.log(`ctemp:`, ctemp)
@@ -38,38 +38,8 @@ navigator.geolocation.getCurrentPosition(function(position) {
           else {
             document.getElementById("windChill").innerHTML= `N/A`;
           }
-          var ctemp2 = parseFloat(data.list[8].main.temp);
-          //ctemp2 = ctemp2 - 50; // test
-          console.log(`ctemp2:`, ctemp2)
-          var wSpeed2= parseFloat(data.list[8].wind.speed);
 
-          if (ctemp2 <= 50 & wSpeed2 > 3.0) {
-            windChill2 = (35.74 + (0.6215 * ctemp2))-(35.75 * Math.pow(wSpeed2,0.16)) + (0.4275*ctemp2*Math.pow(wSpeed2,0.16));
-            windChill2 = Math.round(windChill2);
-        
-            console.log(windChill2);
-        
-            document.getElementById("windChill2").innerHTML= windChill2 + `&deg;F`;
-          }
-          else {
-            document.getElementById("windChill2").innerHTML= `N/A`;
-          }
-          var ctemp3 = parseFloat(data.list[16].main.temp);
-          //ctemp3 = ctemp2 - 50; // test
-          console.log(`ctemp2:`, ctemp3)
-          var wSpeed3 = parseFloat(data.list[16].wind.speed);
-
-          if (ctemp3 <= 50 & wSpeed3 > 3.0) {
-            windChill3 = (35.74 + (0.6215 * ctemp2))-(35.75 * Math.pow(wSpeed3,0.16)) + (0.4275*ctemp3*Math.pow(wSpeed3,0.16));
-            windChill3 = Math.round(windChill3);
-        
-            console.log(windChill3);
-        
-            document.getElementById("windChill3").innerHTML = windChill3 + `&deg;F`;
-          }
-          else {
-            document.getElementById("windChill3").innerHTML = `N/A`;
-          }
+          
 
           displayResults(data); // uncomment when ready
         } else {
